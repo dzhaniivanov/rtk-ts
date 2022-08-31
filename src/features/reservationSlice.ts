48cafd7012eb958addf1936734084c1e6e77dfbc
dzhaniivanov94 <68897146+dzhaniivanov94@users.noreply.github.com>
2022-08-31 15:36:15 +0300

    setup rtk

---
+++ src/features/reservationSlice.ts
@@ -0,0 +1,13 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  value: [],
+};
+
+export const reservationSlice = createSlice({
+  name: "reservations",
+  initialState,
+  reducers: {},
+});
+
+export default reservationSlice.reducer;
